d2e142c3177a73d15b478aa0514165f7af7f1937
Thangiee <Thangiee0@gmail.com>
2016-08-03 15:47:01 -0500

    shut down server with enter key

--- server/src/main/scala/server/Server.scala
+++ server/src/main/scala/server/Server.scala
@@ -7,6 +7,8 @@ import akka.stream.ActorMaterializer
 import play.api.libs.json.{Json, Reads, Writes}
 import share.{Api, ServerConfig}
 
+import scala.io.StdIn
+
 object AutowireServer extends autowire.Server[String, Reads, Writes] {
   def read[Result: Reads](p: String): Result = Json.parse(p).as[Result]
   def write[Result: Writes](r: Result): String = Json.toJson(r).toString()
@@ -27,7 +29,12 @@ object Server extends App {
     }
   }
 
-  Http().bindAndHandle(routes, ServerConfig.interface, ServerConfig.port)
+  val binding = Http().bindAndHandle(routes, ServerConfig.interface, ServerConfig.port)
   println(s"Server listening on ${ServerConfig.interface}:${ServerConfig.port}")
+  StdIn.readLine()
+
+  binding
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ ⇒ system.terminate()) // and shutdown when done
 }
 
